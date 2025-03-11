f3f778d2dbae9ac626de7d410ac2d1fd80a083d7
Lina Ben-Younes <115539807+lina-by@users.noreply.github.com>
2025-03-11 16:56:09 +0100

    Robot Agents v1

--- agents.py
+++ agents.py
@@ -6,7 +6,7 @@ class Knowledge(BaseModel):
    position:Tuple[int, int]
    target_positions:List[Tuple[int, int]]
    my_zone:Tuple[int, int, int, int] # x_min, x_end, y_min, y_end
-   allowed_zone:Tuple[Tuple[int, int]]
+   allowed_zone:Tuple[int, int, int, int]
    carrying:int # 0 if nothing, 
                 # 1 if carrying one of the robot's own color,
                 # 2 if carrying one of the next color
@@ -27,9 +27,25 @@ class Robot(Agent):
         action = self.deliberate() 
         self.model.do(self, action) 
 
+    def get_possible_moves(self):
+        possible_moves = []
+        x,y=self.knowledge.position
+        x_min, x_end, y_min, y_end = self.knowledge.allowed_zone
+        if x > x_min:
+            possible_moves.append((x-1, y))
+        if x < x_end:
+            possible_moves.append((x+1, y))
+        if y > y_min:
+            possible_moves.append((x, y-1))
+        if y < y_end:
+            possible_moves.append((x, y+1))
+        return possible_moves
+        
     def move(self):
-       
-       possible_moves = []
+       possible_moves = self.get_possible_moves()
+       new_position = self.random.choice(possible_moves)
+       self.model.grid.place_agent(self, new_position)
+       self.knowledge.position=new_position
 
     def pickup(self):
        pass
